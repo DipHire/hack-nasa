5b6cfa9c85e271e8ae86039d5eaca6dc5dc108cf
NooB Programmers <47987136+DipHire@users.noreply.github.com>
2019-09-29 18:43:07 +0530

    Add files via upload

---
+++ nasahack.py
@@ -0,0 +1,46 @@
+    
+#!/usr/bin/python
+'''create by Dip Hire'''
+
+import smtplib
+import time
+from os import system
+
+def main():
+   print """
+ 
+   _   _                       _   _            _
+  | \ | | __ _ ___  __ _      | | | | __ _  ___| | __
+  |  \| |/ _` / __|/ _` |_____| |_| |/ _` |/ __| |/ /
+  | |\  | (_| \__ \ (_| |_____|  _  | (_| | (__|   <
+  |_| \_|\__,_|___/\__,_|     |_| |_|\__,_|\___|_|\_\   """
+   print '================================================='
+   print '               coded by Dip Hire                 '
+   print '                  Just For Fun                   '
+   print '================================================='
+
+main()
+print '[1] start the attack'
+print '[2] exit'
+option = input('Enter Option =>')
+if option == 1:
+	 print 'Hacking Nasa............10%  complete'
+            time.sleep(2)
+	 print 'Hacking Nasa............20%  complete'
+	 		time.sleep(2)
+	 print 'Hacking Nasa............40%  complete'
+			time.sleep(2)
+	 print 'Hacking Nasa............60%  complete'
+			time.sleep(2)
+	 print 'Hacking Nasa............80%  complete'
+			time.sleep(2)
+	 print 'Hacking Nasa............100%  complete'
+			time.sleep(2)
+	  print 'You Hacked Nasa Successfully'
+   
+else:
+   system('clear')
+   exit()
+   
+
+
